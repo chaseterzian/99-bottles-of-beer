db028cc82bea712c41b1769e360e033cd84e28b3
ChaseTerzian <chaseterzian@gmail.com>
2015-02-10 00:06:54 -0700

    99 bottles of beer

---
+++ 99bottlesofbeer.rb
@@ -0,0 +1,9 @@
+n = (99)
+while n > 0
+  puts n.to_s + "bottles of beer on the wall, " + n.to_s + " bottles of beer, take one down, pass it around, "
+  n = (n - 1)
+  puts n.to_s + " bottle of beer on the wall"
+end
+
+  
+
